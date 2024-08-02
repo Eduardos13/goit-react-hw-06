1c122c5046e31f1619a68df0e44ec1abd464f1b9
Eduardos13 <edik.kozelsky@gmail.com>
2024-08-01 22:37:35 -0400

    base commit
    created. need to rename later everything

---
+++ src/redux/ something rename later/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  something: true,
+};
+
+export const somethingReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
